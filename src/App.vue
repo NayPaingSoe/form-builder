e1fa24f0f44762a4f4296090a004a9af9b6ff53a
NayPaingSoe <naypaingsoe042@gmail.com>
2025-10-21 13:13:51 +0630

    fix: install tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,10 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="fixed top-0 left-0 right-0 bg-[#22272d] p-4 space-x-2.5">
+      <RouterLink to="/" class="text-white px-6">Form Builder</RouterLink>
+      <RouterLink to="/user" class="text-white">User</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
